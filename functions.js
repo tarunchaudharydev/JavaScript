c24eaa87c85b889a6efa098d3642ad3cf9afdc4c
tarunchaudharydev <tarunchaudhary.dev@gmail.com>
2025-11-15 07:11:34 +0530

    Functions

--- functions.js
+++ functions.js
@@ -4,23 +4,23 @@
 
 let name = "Tarun";
 
-function greet(name) {
-  return `Hello, ${name}`;
-}
+// function greet(name) {
+//   return `Hello, ${name}`;
+// }
 
-console.log(greet(name));
+// console.log(greet(name));
 
 // Function Expression
-const greetExp = function (name) {
-  return `Hi, ${name}`;
-};
-console.log(greetExp(name));
+// const greetExp = function (name) {
+//   return `Hi, ${name}`;
+// };
+// console.log(greetExp(name));
 
 // Arrow Function
-const greetArrow = (name) => {
-  return `Hey, ${name}`;
-};
-console.log(greetArrow(name));
+// const greetArrow = (name) => {
+//   return `Hey, ${name}`;
+// };
+// console.log(greetArrow(name));
 
 // Basic syntax
 // function functionName () {
@@ -106,3 +106,66 @@ const subtract = (a, b) => {
 
 let result3 = subtract(10, 4);
 console.log(result3);
+
+//  function ---> first class citizens
+// In JavaScript, functions are first-class citizens, meaning they can be treated like any other variable. They can be assigned to variables, passed as arguments to other functions, and returned from functions.
+
+// Example: Assigning a function to a variable
+// as argument
+// returned from function
+// we use function in DS
+// property as object
+
+// assign to variable
+let greet = function () {
+  console.log("This is a greeting function");
+};
+greet();
+
+// passing as argument
+function greetMe(greet, fullName) {
+  console.log("Hello", fullName);
+  greet();
+}
+
+greetMe(greet, "Tarun Kumar");
+
+// returning from function
+
+function solve(number) {
+  return function (number) {
+    return number * number;
+  };
+}
+
+let ans = solve(5);
+let finalAnswer = ans(10);
+console.log(finalAnswer);
+
+const arr = [
+  function (a, b) {
+    return a + b;
+  },
+  function (a, b) {
+    return a - b;
+  },
+  function (a, b) {
+    return a * b;
+  },
+];
+
+let first = arr[1];
+let answer = first(5, 10);
+console.log(answer);
+
+let obj = {
+  age: 25,
+  wt: 70,
+  ht: 100,
+  greeting: () => {
+    console.log("Hello");
+  },
+};
+
+console.log(obj.age);
+obj.greeting();
