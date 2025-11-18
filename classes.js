073471b59da52e8bba401a478c58e4cca97bdafb
tarunchaudharydev <tarunchaudhary.dev@gmail.com>
2025-11-18 07:39:11 +0530

    sick

--- classes.js
+++ classes.js
@@ -1,1 +1,21 @@
 // classes in javascript
+class Human {
+  // properties
+  age = 13;
+  wt = 80;
+  ht = 180;
+
+  // behavior
+  walking() {
+    console.log("I am walking");
+  }
+
+  running() {
+    console.log("I am running");
+  }
+}
+
+let obj = new Human();
+console.log(obj.age);
+
+obj.walking();
