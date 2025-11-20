f90522e2b5d10d9707e1335f262c572a6c53f063
tarunchaudharydev <tarunchaudhary.dev@gmail.com>
2025-11-20 07:53:02 +0530

    start

---
+++ dynamic_nature.js
@@ -0,0 +1,42 @@
+console.log("tarun");
+
+// The dynamic nature of JavaScript allows us to add or modify properties and methods of built-in objects at runtime.
+// Here are some examples demonstrating this feature:
+// Example 1: Adding a new method to the Math object
+Math.square = function (x) {
+  return x * x;
+};
+console.log("Square of 5:", Math.square(5)); // Output: 25
+
+// Example 2: Modifying an existing method of the String object
+String.prototype.reverse = function () {
+  return this.split("").reverse().join("");
+};
+
+let obj = {
+  age: 20,
+  wt: 68,
+  ht: 180,
+};
+
+console.log(obj);
+
+obj.color = "black";
+
+console.log(obj);
+
+// cloning object
+// three ways to clone an object
+// using spread operator
+let object = { a: 1, b: 2, c: 3 };
+let cloneobject = { ...object };
+
+console.log(cloneobject);
+
+// using Object.assign()
+let dest = Object.assign({}, object);
+console.log(dest);
+
+// using JSON methods
+let jsonClone = JSON.parse(JSON.stringify(object));
+console.log(jsonClone);
