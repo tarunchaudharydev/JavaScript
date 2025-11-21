89fe4e5a82d2d564c001006a59764075e605182f
tarunchaudharydev <tarunchaudhary.dev@gmail.com>
2025-11-21 22:26:06 +0530

    ill

---
+++ BOM.js
@@ -0,0 +1,6 @@
+// Browser Object Model (BOM) in JavaScript
+// The BOM allows JavaScript to interact with the browser outside of the webpage content.
+// It provides objects like window, navigator, screen, location, and history.
+
+//  we can change html or access using the id, classname, tagname etc.
+// The window object is the global object representing the browser window.
